f8d9e8f2113872c89f2adde95a1f389e50502322
iexist21 <107584749+iexist21@users.noreply.github.com>
2022-07-11 17:06:01 -0400

    Made it so that it checks when the input file is valid
    It also does the input file, and leaves a temp.lua, which later will be cleaned up

--- lua-.lua
+++ lua-.lua
@@ -1,8 +1,19 @@
+require('lib/OSTYPE')
+function DelTemp()
+    if os.name == "Windows" then
+        os.execute('del "temp.lua"') 
+    elseif os.name == "Linux" then
+        os.execute('rm temp.lua')
+    elseif os.name == "MacOS" then 
+        os.execute('rm temp.lua')
+
+    end
+end
 -- For now, it will only inset functions, not replace the code at that area. | do not expect an experince like Typescript
-new = "function new(Table) o = {} setmetatable(o, Table) Table.__index = Table return o end \n "
+local new = "function new(Table) o = {} setmetatable(o, Table) Table.__index = Table return o end \n "
 -- timing in seconds
-sleep = "\n function sleep(time) starttime = os.time() repeat until os.time() > starttime + time end \n"
-prot = "\n function protect(tbl) return setmetatable({}, { __index = tbl, __newindex = function(t, key, value) error('attempting to edit a protected table ' .. tostring(key) ..  ' to ' .. tostring(value), 2) end }) end \n"
+local sleep = "\n function sleep(time) starttime = os.time() repeat until os.time() > starttime + time end \n"
+local prot = "\n function protect(tbl) return setmetatable({}, { __index = tbl, __newindex = function(t, key, value) error('attempting to edit a protected table ' .. tostring(key) ..  ' to ' .. tostring(value), 2) end }) end \n"
 defaultinserts = {
 "math.randomseed(os.time()) \n"
 };
@@ -22,6 +33,13 @@ if #data < 0 then
     io.write("You somehow have negitive text, maybe try again????")
     os.exit() 
 end
+FullFileOUT = defaultinserts[1]..prot..new..sleep..data.."\n"
+f = io.open("temp.lua", "w")
+f:write(FullFileOUT)
+f:close()
+local TempFileRS = io.open("temp.lua", "r"):read("*a")
+pcall(TempFileRS)
+DelTemp()
 Write = io.open(OUTFILE, "w")
 Write:write(defaultinserts[1])
 Write:write(prot)
